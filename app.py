08b7cfad85bc796498d156b77f9c4fb269ceccd4
Mishrashivam-SKM <mishrashivamskm7@gmail.com>
2023-11-02 10:31:16 +0530

    this is my very first time to push a code in github

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask , render_template, request
+import requests 
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template("index.html")
+
+@app.route('/weatherapp', methods=['POST'])
+def get_weatherdata():
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+    parameter = {
+        'q': request.form.get("city"),
+        'appid': request.form.get("appid"),
+        'units' : request.form.get("units")
+        }
+    data_request = requests.get(url , params= parameter)
+    data = data_request.json()
+    return f"data :{data}"
+
+
+if __name__ == '__main__':
+    app.run(host= "0.0.0.0" , port = 5002)
